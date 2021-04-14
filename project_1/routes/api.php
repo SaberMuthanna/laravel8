2ee80580262e41d75ddc9f95dc6742e546ca4d4c
Saber Muthanna <34119145+SaberMuthanna@users.noreply.github.com>
2021-04-14 17:46:59 +0300

    project1

--- project_1/routes/api.php
+++ project_1/routes/api.php
@@ -18,3 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get("/user", function () {
+    return "saber almuthanna";
+});
+
+route::get("/user/{name}", function($name){
+    return "My Nmae is :".$name;
+});
